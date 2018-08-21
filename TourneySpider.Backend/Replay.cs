d922c0e7a2844f13341ef5e2309f6aa5eed1be10
Alexander Schwoch <alex@palc.de>
2018-08-22 01:29:59 +0200

    added a replay finder (aiming to find a match id automagically)

---
+++ TourneySpider.Backend/Replay.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TourneySpider.Backend
+{
+	public class Replay
+	{
+		public static Replay Parse( string directory )
+		{
+			string[] parts = directory.Split( new[] {'.'} );
+
+			string type = parts[ 2 ]; // "official", "custom", ... ?
+
+			string version = parts[ 3 ]; // "2018-05"
+
+			string region = parts[ 4 ]; // "eu", ...
+			string gameMode = parts[ 5 ]; // "solo", "solo-fpp"
+
+			int year = Convert.ToInt32( parts[ 6 ] ); // TODO what if starting with 4 instead 2?
+			int month = Convert.ToInt32( parts[ 7 ] );
+			int day = Convert.ToInt32( parts[ 8 ] ); 
+			var date = new DateTime(year, month, day);
+
+			string id = parts[ 9 ].Substring( 0, 36 );
+			var guid = Guid.Parse( id );
+
+			return new Replay()
+			{
+				FolderName = directory,
+				Type = type,
+				Version = version,
+				Region = region,
+				Mode = gameMode,
+				Date = date,
+				Id = guid
+
+			};
+		}
+
+		public string FolderName { get; set; }
+
+		public string Region { get; set; }
+
+		public string Mode { get; set; }
+
+		public Guid Id { get; set; }
+
+		public DateTime Date { get; set; }
+
+		public string Version { get; set; }
+
+		public string Type { get; set; }
+	}
+}
---
+++ TourneySpider.Backend/ReplayFinder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourneySpider.Backend
+{
+	public class ReplayFinder
+	{
+		private const string DefaultPath = @"%localappdata%\TslGame\Saved\Demos";
+
+		public ReplayFinder()
+		{
+			SourcePath = Environment.ExpandEnvironmentVariables( DefaultPath );
+		}
+
+		public string SourcePath { get; set; }
+
+		public IList<Replay> GetReplays()
+		{
+			var result = new List<Replay>();
+
+			foreach ( var path in Directory.EnumerateDirectories( SourcePath ) )
+			{
+				string folderName = Path.GetFileName( path );
+
+				if ( folderName != null && folderName.StartsWith( "match.bro" ) )
+				{
+					var newReplay = Replay.Parse( folderName );
+					result.Add( newReplay );
+				}
+			}
+
+			return result;
+		}
+
+	}
+}
