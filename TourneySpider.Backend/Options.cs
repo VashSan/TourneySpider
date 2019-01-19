97c29740d92f8857546733a578007a163a6250a0
Alexander Schwoch <alex@palc.de>
2019-01-19 22:32:30 +0100

    updated options

--- TourneySpider.Backend/Options.cs
+++ TourneySpider.Backend/Options.cs
@@ -1,4 +1,5 @@
-﻿using CommandLine;
+﻿using System;
+using CommandLine;
 
 namespace TourneySpider.Backend
 {
@@ -7,9 +8,10 @@ namespace TourneySpider.Backend
 		[Option( 'm', "matchid", HelpText = "The match id to analyse.", Required = true )]
 		public string MatchId { get; set; }
 
-		[Option( 'p', "platform", HelpText = "Specify the platform the match took place on.", Default = "PC" )]
+		[Option( 'p', "platform", HelpText = "Specify the platform the match took place on.", Default = "Steam" )]
 		public string Platform { get; set; }
 
+		[Obsolete("Not required anymore, might be removed")]
 		[Option( 'r', "region", HelpText = "Specify the region the match took place in.", Default = "Europe" )]
 		public string Region { get; set; }
 	}
