70c124361e721a9372fbb1579de3927a907a9702
Alexander Schwoch <alex@palc.de>
2019-01-19 22:28:04 +0100

    update pubg.net library updated to new api

--- TourneySpider.Backend/PubgSpider.cs
+++ TourneySpider.Backend/PubgSpider.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using Pubg.Net;
+using Pubg.Net.Exceptions;
 
 namespace TourneySpider.Backend
 {
@@ -17,23 +20,54 @@ namespace TourneySpider.Backend
 
 		public IEnumerable<PlayerResult> GetResult()
 		{
-			PubgRegion region = GetPubgRegion();
+			PubgPlatform platform = GetPubgPlatform();
 
-			var matchService = new PubgMatchService();
-			PubgMatch match = matchService.GetMatch( region, myOptions.MatchId );
+			PubgMatch match = GetMatch( platform );
+			if ( match == null )
+			{
+				yield break;
+			}
 
-			
-			var result = new List<PlayerResult>();
+			foreach ( PlayerResult playerResult in GetPlayerResultFromMatch( match ) )
+			{
+				yield return playerResult;
+			}
+		}
+
+		private static IEnumerable<PlayerResult> GetPlayerResultFromMatch( PubgMatch match )
+		{
 			foreach ( PubgRoster roster in match.Rosters )
 			{
 				foreach ( PubgParticipant participant in roster.Participants )
 				{
-					var player = new PlayerResult( participant.Stats.Name, roster.Stats.Rank, participant.Stats.Kills);
-					result.Add( player );
+					var player = new PlayerResult( participant.Stats.Name, roster.Stats.Rank, participant.Stats.Kills );
+					yield return player;
 				}
 			}
+		}
 
-			return result;
+		private PubgMatch GetMatch( PubgPlatform platform )
+		{
+			var matchService = new PubgMatchService();
+			PubgMatch match;
+			try
+			{
+				match = matchService.GetMatch( platform, myOptions.MatchId );
+			}
+			catch ( PubgNotFoundException e ) when ( e.HttpStatusCode == HttpStatusCode.NotFound )
+			{
+				// Errorcode not set, we wont parse strings though!
+				Console.WriteLine( $"Can not find match. (Id = {myOptions.MatchId})" );
+				return null;
+			}
+			catch ( Exception e )
+			{
+				Console.WriteLine( $"Can not process match. (Id = {myOptions.MatchId})" );
+				Console.WriteLine( e.Message );
+				return null;
+			}
+
+			return match;
 		}
 
 		private PubgRegion GetPubgRegion()
@@ -51,5 +85,19 @@ namespace TourneySpider.Backend
 			WasRegionReset = true;
 			return defaultRegion;
 		}
+
+		private PubgPlatform GetPubgPlatform()
+		{
+			string s = myOptions.Platform;
+			if ( Enum.TryParse( s, true, out PubgPlatform platform ) )
+			{
+				return platform;
+			}
+
+			WasPlatformReset = true;
+			return PubgPlatform.Steam;
+		}
+
+		public bool WasPlatformReset { get; private set; }
 	}
 }
